27e4a93d86602be6d97939aead6ef24e5701d610
Amritpal Singh <amrit3701@gmail.com>
2017-11-05 17:22:04 +0530

    fixme bug

--- Webapp/urls.py
+++ Webapp/urls.py
@@ -23,7 +23,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'', include('src.urls')),
-    url(r'^$', TemplateView.as_view(template_name='src/index.html')),
+    url(r'^$', views.home, name='login'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
