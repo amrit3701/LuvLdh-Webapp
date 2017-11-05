9ac851eedddc70782405fdaacb15163d372ea34a
Amritpal Singh <amrit3701@gmail.com>
2017-11-05 15:53:49 +0530

    Implemented content writing

--- src/urls.py
+++ src/urls.py
@@ -7,5 +7,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
         #directs to index veiw
-        url(r'^upload/$', views.upload, name='upload'),
+        url(r'^upload_photo/$', views.upload_photo, name='upload_photo'),
+        url(r'^upload_contentwriting/$', TemplateView.as_view(template_name='src/contentwriting.html')),
+        url(r'^upload_contentwriting_postlink$', views.upload_contentwriting, name='upload_contentwriting'),
 ]
--- src/views.py
+++ src/views.py
@@ -9,9 +9,9 @@ from django.contrib.auth.decorators import login_required
 from decouple import config
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
+from wand.image import Image
 
-
-#import ipdb
+import ipdb
 @login_required
 def home(request):
     #ipdb.set_trace()
@@ -20,7 +20,7 @@ def home(request):
 import facebook
 
 @login_required
-def upload(request):
+def upload_photo(request):
     fi = request.FILES.get('userfile')
     message = request.POST.get('message')
     social = request.user.social_auth.get(provider='facebook')
@@ -28,15 +28,13 @@ def upload(request):
     api = facebook.GraphAPI(access_token)
     user_id = api.get_object('me', fields="id")['id']
     username = api.get_object('me', fields="name")['name']
-    url_share = request.POST.get('message')
-    #ipdb.set_trace()
-    fs = FileSystemStorage(location=settings.MEDIA_ROOT+"/profile")
-    savedFilename = user_id + "_pic." + fi.name.split('.')[-1]
+    profile_link = api.get_object("me", fields="link")['link']
+    fs = FileSystemStorage(location=settings.MEDIA_ROOT+"/photography/")
+    savedFilename = user_id + "." + fi.name.split('.')[-1]
     filename = fs.save(savedFilename, fi)
-    savedFilenameURL = request.get_host() + "/media/profile/" + savedFilename
-    #ipdb.set_trace()
+    savedFilenameURL = request.get_host() + "/media/photography/" + savedFilename
     graph = facebook.GraphAPI(access_token = config('PAGE_ACCESS_TOKEN'))
-    msg = "Username: " + username + "\nCategory: Photography\n" + "Description: " + message
+    msg = "Username: " + username + "(" + profile_link + ")\nCategory: Photography\n" + "Description: " + message
     attachment =  {
         #'name': 'Link name',
         'link': savedFilenameURL,#"http://lab.gdy.club:7777/media/1580271875386906_pic.png", #savedFilenameURL,
@@ -49,9 +47,43 @@ def upload(request):
     fbpostURL = URLofSharedPost(status)
     return HttpResponse("Check <a href='" + fbpostURL + "'>post</a>")
 
+@login_required
+def upload_contentwriting(request):
+    fi = request.FILES.get('userfile')
+    message = request.POST.get('message')
+    social = request.user.social_auth.get(provider='facebook')
+    access_token = social.extra_data['access_token']
+    api = facebook.GraphAPI(access_token)
+    user_id = api.get_object('me', fields="id")['id']
+    username = api.get_object('me', fields="name")['name']
+    profile_link = api.get_object("me", fields="link")['link']
+    fs = FileSystemStorage(location=settings.MEDIA_ROOT+"/contentwriting/")
+    savedFilename = user_id + "." + fi.name.split('.')[-1]
+    filename = fs.save(savedFilename, fi)
+    #ipdb.set_trace()
+    savedFilenameURL = request.get_host() + "/media/contentwriting/" + savedFilename
+    imageLocation = settings.MEDIA_ROOT + "/contentwriting/" + user_id + ".jpg"
+    imageURL = request.get_host() + "/media/contentwriting/" + user_id + ".jpg"
+    pdfLocation = fs.base_location+savedFilename
+    pdf_to_image(pdfLocation+"[0]", imageLocation)
+    graph = facebook.GraphAPI(access_token = config('PAGE_ACCESS_TOKEN'))
+    msg = "Username: " + username + "(" + profile_link + ")\nCategory: Photography\n" + "Description: " + message
+    attachment =  {
+        #'name': 'Link name',
+        'link': savedFilenameURL,#"http://lab.gdy.club:7777/media/1580271875386906_pic.png", #savedFilenameURL,
+        #'caption': 'Check out this example',
+        #'description': 'This is a longer description of the attachment',
+        'picture': imageURL,#"http://lab.gdy.club:7777/media/1580271875386906_pic.png" #savedFilenameURL
+    }
+    status = graph.put_wall_post(msg, attachment)
+    fbpostURL = URLofSharedPost(status)
+    return HttpResponse("Check <a href='" + fbpostURL + "'>post</a>")
+
+
 def URLofSharedPost(status):
     post_id = status['id'].split('_')[-1]
     return "https://www.facebook.com/permalink.php?story_fbid="+ post_id +"&id=126210538066873"
 
-def URLofUserProfile(user_id):
-    return "https://www.facebook.com/profile.php?id=" + user_id
+def pdf_to_image(pdf_location, img_location):
+    with Image(filename=pdf_location) as img:
+        img.save(filename=img_location)
